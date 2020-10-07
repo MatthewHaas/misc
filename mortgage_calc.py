511e95dfcce0c00fb44f40d6e57ac1e873e1b041
Matthew Haas <matthew.haas1@gmail.com>
2020-10-07 11:48:02 -0500

    Updated, amount of decrease is still slightly off.

--- mortgage_calc.py
+++ mortgage_calc.py
@@ -6,37 +6,42 @@ rate = (Decimal(input("Enter your yearly interest rate:"))/100)
 term = int(input("Enter the length of your mortgage (in years):"))
 
 # Count is so that when calc_mortgage() is called repeatedly (which it is), it won't continually decrease the monthly payment amount.
-count = 0
+#count = 0
 
 def calc_mortgage():
-	global principle,payment,count
-	if count == 0:
-		payment = principle*((rate/12)*((1+(rate/12))**(term*12)))/(((1+(rate/12))**(term*12))-1)
-		payment = round(payment, 2)
-		count += 1
-	return print('Your monthly payment is: $', payment)
+	payment = principle*((rate/12)*((1+(rate/12))**(term*12)))/(((1+(rate/12))**(term*12))-1)
+	payment = round(payment, 2)
 	return payment
+	
 
 def calc_princ_payment():
-	global principle,payment,principle_payment,interest_payment
-	return calc_mortgage()
+	global payment,principle
 	interest_payment = (principle*(rate))/12
 	principle_payment = Decimal(payment) - Decimal(interest_payment)
-	return principle,principle_payment,interest_payment
+	return principle_payment
 
-def apply_payment(principle):
-	global principle_payment
-	return calc_princ_payment()
+def apply_payment():
+	global principle,principle_payment
+	principle_payment = calc_princ_payment()
 	principle = principle - principle_payment
-	return principle,principle_payment
+	return principle
 	
 amoritization = {}
-for year in range(2020,2051,1):
-	for month in ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]:
-		if principle > 0:
-			apply_payment(principle)
-			x = month + str(year)
-			amoritization[x] = float(principle)
-			print(month + " " + str(year) + " " + str(principle))
-		else:
-			pass
+#count = 0
+payment = calc_mortgage()
+while principle > 0:
+	for year in range(2020,2050,1):
+		for month in ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]:
+			if principle > payment:
+				principle_payment = calc_princ_payment()
+				principle = apply_payment()
+				x = month + str(year)
+				amoritization[x] = round(float(principle),2)
+				print(month + " " + str(year) + " " + str(round(principle,2)))
+			elif principle < payment:
+				principle = 0
+				x = month + str(year)
+				amoritization[x] = round(float(principle),2)
+				print(month + " " + str(year) + " " + str(principle))
+				break
+print(amoritization)
