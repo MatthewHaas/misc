89e4b75f0a0a3504845106a34541e10f5d1af048
Matthew Haas <matthew.haas1@gmail.com>
2021-09-29 21:26:00 -0500

    Split day and time columns

--- data_analysis_case_studies/clean_data.R
+++ data_analysis_case_studies/clean_data.R
@@ -22,5 +22,18 @@ data_dedup[, c("end_date", "end_time") := tstrsplit(ended_at, " ", fixed = TRUE)
 data_dedup[, started_at := NULL]
 data_dedup[, ended_at := NULL]
 
+# Make new columns for end year, month, and day
+data_dedup[, c("end_year", "end_month", "end_day") := tstrsplit(end_date, "-", fixed = TRUE)]
+
+# Make new columns for end hour, minute, and second
+data_dedup[, c("end_hour", "end_minute", "end_second") := tstrsplit(end_time, ":", fixed = TRUE)]
+
+# Remove the "second" columns--I don't think they're necessary
+data_dedup[, start_second := NULL]
+data_dedup[, end_second := NULL]
+
+# Assign a better name to "member_casual" column
+setnames(data_dedup, "member_casual", "membership_type")
+
 # Save data
 fwrite(data_dedup, file = "clean_bike_trip_data.csv", col.names = TRUE, row.names = FALSE)
